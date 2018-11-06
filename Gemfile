52f450521b0ab38e4753110af89fc41b657132a5
ruthmesfun <ruth.mesfun@gmail.com>
2018-11-06 15:58:36 -0500

    Im done

--- Gemfile
+++ Gemfile
@@ -2,7 +2,9 @@
 source "https://rubygems.org"
 
 # Database
-gem 'sqlite3'
+gem 'sqlite3', '~> 1.3', '>= 1.3.13'
+
+gem 'sqlite3-ruby', '~> 1.3', '>= 1.3.3'
 
 # Testing
 gem 'rspec'
---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 8, "F", "black", "still", false);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES("Janet", 9, "F", "black", "calm", true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES("Don", 6, "F", "brown", "rough", true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES("Crystal", 9, "F", "white", "happy", true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES("Nicolette", 1, "F", "silver", "curious", true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES("Isiah", 5, "M", "rainbow", "excited", true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES("Matt", 2, "F", "transparent", "angry", true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES("Mike", 10, "F", "gold", "wild", false);
